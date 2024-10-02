b399dac54b86d6c139ad52c2ea7020720ba370b7
Your Name <you@example.com>
2024-10-02 07:27:22 +0000

    start с докером

---
+++ next-app/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function ({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <div className="bg-red-200">Главная 12exxxxxx</div>
+    )
+  }
+  
